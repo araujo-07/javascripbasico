61701684df8e0421d7bb2fc23ca59f7f93b27e76
miguel araujo <miguel.araujo680@asys.edu.co>
2024-09-12 21:23:22 -0500

    cambios semana7

---
+++ semana 7/concatenacion/index.js
@@ -0,0 +1,26 @@
+const cadena1 = "hola "
+const cadena2 = " mundo"
+
+console.log(cadena1 + cadena2);
+
+const nombre = "miguel";
+const mensaje = "hablame";
+const saludo = "bien o que";
+const imprimir = nombre + " " + mensaje +" "+saludo
+
+console.log (imprimir)
+
+const nombre2 = "miguel";
+const mensaje2 = "hablame";
+const saludo2 = "bien o que";
+const imprimir2 = nombre + " " + mensaje +" "+saludo
+
+const imprimir2saltodelinea = `${nombre2}
+ ${mensaje2}
+ ${saludo2}`
+console.log(imprimir2)
+console.log(imprimir2saltodelinea)
+
+
+
+
